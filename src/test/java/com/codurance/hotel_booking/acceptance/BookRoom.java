07b9b8aa2e5ee94cadcac567bd619304f11c83ae
upsd <up648421@myport.ac.uk>
2019-06-10 13:11:02 +0100

    Move variables to fields to minimise noise in acceptance test.

--- src/test/java/com/codurance/hotel_booking/acceptance/BookRoom.java
+++ src/test/java/com/codurance/hotel_booking/acceptance/BookRoom.java
@@ -6,6 +6,7 @@ import com.codurance.hotel_booking.booking_policy.BookingPolicyService;
 import com.codurance.hotel_booking.company.CompanyService;
 import com.codurance.hotel_booking.hotel.HotelService;
 import com.codurance.hotel_booking.hotel.Room;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -24,21 +25,30 @@ class BookRoom {
     private static final long HOTEL_ID = 4l;
     private static final Room ROOM_TYPE = Room.JUNIOR_SUITE;
     private static final int QUANTITY = 1;
+    private CompanyService companyService;
+    private HotelService hotelService;
+    private BookingPolicyService bookingPolicyService;
+    private BookingService bookingService;
 
-    @Test
-    void when_there_are_no_constraints() {
-        CompanyService companyService = aCompanyService().build();
-        companyService.addEmployee(COMPANY_ID, EMPLOYEE_ID);
-        HotelService hotelService = aHotelService().build();
-        hotelService.setRoomType(HOTEL_ID, ROOM_TYPE, QUANTITY);
-        BookingPolicyService bookingPolicyService = aBookingPolicyService().build();
-        bookingPolicyService.setEmployeePolicy(EMPLOYEE_ID, ROOM_TYPE);
-        LocalDate checkInDate = LocalDate.of(2019, 02, 1);
-        BookingService bookingService = aBookingService()
+    @BeforeEach
+    void setUp() {
+        companyService = aCompanyService().build();
+        hotelService = aHotelService().build();
+        bookingPolicyService = aBookingPolicyService().build();
+
+        bookingService = aBookingService()
                 .withCompanyService(companyService)
                 .withHotelService(hotelService)
                 .withBookingPolicyService(bookingPolicyService)
                 .build();
+    }
+
+    @Test
+    void when_there_are_no_constraints() {
+        companyService.addEmployee(COMPANY_ID, EMPLOYEE_ID);
+        hotelService.setRoomType(HOTEL_ID, ROOM_TYPE, QUANTITY);
+        bookingPolicyService.setEmployeePolicy(EMPLOYEE_ID, ROOM_TYPE);
+        LocalDate checkInDate = LocalDate.of(2019, 02, 1);
 
         Booking booking = bookingService.book(EMPLOYEE_ID, HOTEL_ID, ROOM_TYPE, checkInDate, checkInDate.plusDays(1));
 
